96a5af31b17e9e70e1ce7ce3f6a3ccb86e6c7be3
Nicolay <krjuchkovkolja@mail.ru>
2021-06-29 12:25:08 +0300

    Сщтештгу  work

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,15 @@
 require('./bootstrap');
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 import Vue from "vue";
 
-// Пример компонента
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.component('example-component', require('./components/ExampleComponent').default);
 Vue.component('header-component', require('./components/header').default);
+Vue.component('table-component', require('./components/table').default);
 
 new Vue({
     el: '#app',
