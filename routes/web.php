1692825497a41145bce50e85f5db142d181c2fd8
Nicolay <krjuchkovkolja@mail.ru>
2021-07-07 10:51:05 +0300

    GetData  work

--- routes/web.php
+++ routes/web.php
@@ -67,21 +67,28 @@ Route::get('test2',function(){
     $collection = CompanyName::select('id','xml_feed')->where('user_id', Auth::user()->id)->get();
     $array_xml = [];
     $array_new = [];
+    $array_xml_feed = [];
     foreach($collection as $value){
         $array_xml[$value['id']] =  $value['xml_feed'];
     }
     foreach($array_xml as $index =>$item_xml){
-        $xml_feed= CurrentXml::select('id_flat','id_user','id_company')
+        $xml_feed= CurrentXml::select('id_flat','bet','id_user','id_company','name_agent')
                                 ->where('id_user', Auth::user()->id)
                                 ->where('id_company', $index)
                                 ->get()->toArray();
-        $xml_feed_copy = $xml_feed;
         $xml = simplexml_load_file($item_xml);
         $array = json_decode(json_encode($xml),TRUE);
         //id_flat,bet,name_agent,id_user,id_company
+        foreach($xml_feed as $index_xml => $current_xml_feed){
+            $array_xml_feed[$current_xml_feed['id_flat']]['id_company']= $current_xml_feed['id_company'];
+            $array_xml_feed[$current_xml_feed['id_flat']]['id_flat']= $current_xml_feed['id_flat'];
+            $array_xml_feed[$current_xml_feed['id_flat']]['bet']= $current_xml_feed['bet'];
+            $array_xml_feed[$current_xml_feed['id_flat']]['id_user']= $current_xml_feed['id_user'];
+            $array_xml_feed[$current_xml_feed['id_flat']]['name_agent']= $current_xml_feed['name_agent'];
+        }
+        $array_xml_feed_copy = $array_xml_feed;
         foreach($array['object'] as $item => $current_item) {
             $current_array['id_flat'] = (int)$current_item['ExternalId'];
-            $current_array['id_user'] = Auth::user()->id;
             $current_array['id_company'] = $index;
             if(array_key_exists('Bet', $current_item)){
                 $current_bet = $current_item['Bet'];
@@ -89,37 +96,33 @@ Route::get('test2',function(){
                 $current_bet = 0;
             }
             $current_agent_name = $current_item['SubAgent']['FirstName'].' '.$current_item['SubAgent']['LastName'];
-            //Сборка массива с новыми эллементами
-            $array_new_item = [];
-            $array_new_item['id_flat'] = $current_array['id_flat'];
-            $array_new_item['id_user'] = $current_array['id_user'];
-            $array_new_item['id_company'] = $current_array['id_company'];
-            $array_new_item['bet'] =  $current_bet;
-            $array_new_item['name_agent'] = $current_agent_name;
-            array_push($array_new, $array_new_item);
-            
-            foreach($xml_feed as $index_xml => $current_xml_feed){
-                if($current_array == $current_xml_feed){
-                    CurrentXml::where('id_flat', '=', $current_array['id_flat'])
-                        ->where('id_company', '=', $current_array['id_company'])
-                        ->where('id_user', $current_array['id_user'])
-                        ->update(array(
-                            'bet' => $current_bet,
-                            'name_agent' =>$current_agent_name
-                        ));
-                    unset($xml_feed[$index_xml]);
-                }
-                else{
-                    continue;
-                }
+            if(array_key_exists($current_array['id_flat'], $array_xml_feed)){
+                CurrentXml::where('id_flat', '=', $current_array['id_flat'])
+                    ->where('id_company', '=', $current_array['id_company'])
+                    ->where('id_user', Auth::user()->id)
+                    ->update(array(
+                        'bet' => $current_bet,
+                        'name_agent' =>$current_agent_name
+                    ));
+                unset($array_xml_feed[$current_array['id_flat']]);
             }
+            else{
+                $newObject = CurrentXml::create(array(
+                    'id_flat' =>$current_array['id_flat'],
+                    'bet'=> $current_bet,
+                    'id_user'=> Auth::user()->id,
+                    'id_company'=> $current_array['id_company'],
+                    'name_agent'=>$current_agent_name
+                ));
+                $newObject->save();
+            } 
         }
-        return $xml_feed;
-        // foreach($array_new as $array_new_elem){
-        //     foreach($xml_feed_copy as $xml_feed_cop_elem){
-        //         if($array_new_elem == )
-        //     }
-        // }
+        foreach($array_xml_feed as $current_array_xml_feed_index => $current_array_xml_feed){
+            CurrentXml::where('id_flat', $current_array_xml_feed_index)
+                        ->where('id_user', Auth::user()->id)
+                        ->where('id_company', $current_array_xml_feed['id_company'])
+                        ->delete();
+        }    
     }
     
 });
